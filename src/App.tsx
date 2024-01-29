a98970610ad09d0bee21c0ededb1004d55fee4f1
Gouthami Matavalam <gouthami.matavalam@worldline.com>
2024-01-29 17:15:02 +0530

    Commit Changes include:
    - Cleaned initally commited code
    - Added functionality to GET data from TheMovieDB for popular movies
    - Listed movies with tiles

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+//Considered necessary information to populate the data
+interface Movies {
+  id: number;
+  title: string;
+  poster_path: string;
+  release_date: string;
+}
 
+function App() {
+
+  const [movies, setMovies] = useState<Movies[]>([]);
+
+  const options = {
+    method: 'GET',
+    url: 'https://api.themoviedb.org/3/movie/popular',
+    params: {
+      api_key: '1bcbd71fd85a1704d859620566e0409b'
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  const fetchData = () => {
+    axios(options).then((response) => {
+      const result = response.data.results;
+      setMovies(result);
+    }).catch(err => console.log(err));
+  }
+
+  //Id is assumed as unique id, considered as key
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {movies.map((items) => (<div className="movieContainer" key={items.id}>
+        <h1>{items.title}</h1>
+      </div>)
+      )}
+    </div>
   )
 }
 
-export default App
+export default App  
