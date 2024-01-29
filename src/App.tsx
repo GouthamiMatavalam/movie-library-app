9bd15a5edd17b34a9c0d6316d58d61d653665c7d
Gouthami Matavalam <gouthami.matavalam@worldline.com>
2024-01-29 17:22:40 +0530

    Added changes to display images and date

--- src/App.tsx
+++ src/App.tsx
@@ -34,11 +34,17 @@ function App() {
     }).catch(err => console.log(err));
   }
 
+  const images_api = 'https://image.tmdb.org/t/p/w300'
+
   //Id is assumed as unique id, considered as key
   return (
     <div>
       {movies.map((items) => (<div className="movieContainer" key={items.id}>
         <h1>{items.title}</h1>
+        {items.poster_path && (
+          <img src={images_api + items.poster_path} alt={items.title} />
+        )}
+        <p>{items.release_date}</p>
       </div>)
       )}
     </div>
